1d3ed95750250c133879171c81af86f0ba984674
Job Finder IPT 101 <adrianhinayon@gmail.com>
2023-11-18 13:19:17 +0800

    Job Finder

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,25 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private EditText username, password;
+    private String correct_username = "admin";
+    private String correct_password = "admin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        username=findViewById(R.id.inputEmail);
+        password=findViewById(R.id.inputPassword);
         loginButton=findViewById(R.id.btnLogin);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, activity_homepage.class));
+                if (TextUtils.isEmpty(username.getText().toString()) && TextUtils.isEmpty(password.getText().toString())){
+                    Toast.makeText(MainActivity.this, "FAILED LOGIN!", Toast.LENGTH_SHORT).show();
+                } else if (username.getText().toString().equals(correct_username)){
+                    if (password.getText().toString().equals(correct_password)){
+                        Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, activity_homepage.class));
+                    }else {
+                        Toast.makeText(MainActivity.this, "Invalid Username/Password", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else{
+                    Toast.makeText(MainActivity.this, "Invalid Username/Password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
